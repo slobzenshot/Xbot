c30096f72e4318b7c393ad5ec0fced31298b92cd
slobzenshot <robbears550@gmail.com>
2026-03-27 09:45:15 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Xbox 24/7 HERE TO HELP",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#3D0B16", 
+      secondary: "#0E2B1F", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
